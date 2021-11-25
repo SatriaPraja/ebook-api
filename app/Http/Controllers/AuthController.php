3ee088186afc13ba6c6a9700d48854cd8e639b5c
SatriaPraja <satriadutapc@gmail.com>
2021-11-25 20:27:35 +0700

    challenge

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,17 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Hash;
+
 class AuthController extends Controller
 {
-    public function me(){
-        return[
-            'NIS' => '3103119173',
-            'Name' => 'Satria Duta Praja',
-            'Phone' => '081215437072',
-            'Class' => 'XII RPL 6/02',
-        ];
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8',
+        ]);
+
+            $user = User::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => Hash::make($validatedData['password']),
+            ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
+use App\Http\Controllers\AuthController;
+
+
+
+
 /**
  * Basic formula pembuatan route
  */
@@ -15,12 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/me','App\Http\Controllers\AuthController@me');
 
 
-Route::get('/book', [BookController::class, 'index']);
-Route::post('/book', [BookController::class, 'store']);
-Route::get('/book/{id}', [BookController::class, 'show']);
-Route::put('/book/{id}', [BookController::class, 'update']);
-Route::delete('/book/{id}', [BookController::class, 'destroy']);
-Route::resource('book', BookController::class)->except('edit', 'create');
+Route::resource("books", "BookController");
+Route::resource("authors", "AuthorController");
